58ef9501dffaeb53860bf2a2f6d77480c88e0a7c
Vladis <vladisrud@gmail.com>
2022-07-03 00:07:00 +0300

    Initial Commit

---
+++ Kiitos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kiitos
+//
+//  Created by Влад Руденко on 03.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
